519ef1bc7e8e92251b1d049f2fb88cfb457dcc5e
Luca Galbusera <luca.galbusera@evotec.com>
2023-12-17 19:28:48 +0100

    [frontend] Component containing the options
    For the moment, it only contains the option to add a new task.

---
+++ frontend/src/components/TodoOptions.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import IconPlus from '@/icons/IconPlus.vue'
+import NewTodo from './NewTodo.vue'
+
+const modal_id = 'newTask'
+</script>
+
+<template>
+  <button
+    type="button"
+    class="btn btn-light"
+    data-bs-toggle="modal"
+    :data-bs-target="`#${modal_id}`"
+  >
+    <IconPlus />
+  </button>
+  <NewTodo :modal_id="modal_id" />
+</template>
