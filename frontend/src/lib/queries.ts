0d95e0c51efdd238e845b72719f4c630f4977040
Luca Galbusera <luca.galbusera@evotec.com>
2023-12-17 19:18:24 +0100

    [frontend] Query all tasks via GraphQL

--- frontend/src/lib/queries.ts
+++ frontend/src/lib/queries.ts
@@ -1,5 +1,6 @@
 import { GRAPHQL_QUERY_NAME, GRAPHQL_ADD_NAME, GRAPHQL_DELETE_NAME, API_URL } from './config'
-import type { graphQLTaskResponse } from './types'
+import { graphqlToTaskInput } from './converters'
+import type { TaskInput, graphQLTaskResponse } from './types'
 
 type GraphQLRespQuery = {
   data: {
@@ -32,4 +33,31 @@ const makeGraphQLRequest = function (body: string) {
 
   const response = fetch(API_URL, req_options)
   return response
+}
+
+export const queryAllTasks = async function (query_name: string): Promise<TaskInput[]> {
+  const fields = [
+    'id',
+    'title',
+    'status',
+    'description',
+    'dateTimestamp',
+    'startTimestamp',
+    'endTimestamp',
+    'goal'
+  ]
+  const query = `query ${query_name} {
+        ${GRAPHQL_QUERY_NAME} {
+          ${fields.join('\n')}
+        }
+    }`
+  const response = await makeGraphQLRequest(query)
+  if (!response.ok) {
+    return []
+  }
+
+  const response_json: GraphQLRespQuery = await response.json()
+  const tasks_ql = response_json.data.queryTask
+  const tasks_converted = tasks_ql.map((task) => graphqlToTaskInput(task))
+  return tasks_converted
 }
