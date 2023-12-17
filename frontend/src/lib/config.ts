429f96b0d1a51775fd5aa55be5d1913acf25221a
Luca Galbusera <luca.galbusera@evotec.com>
2023-12-17 19:06:44 +0100

    [frontend] Global app config

---
+++ frontend/src/lib/config.ts
@@ -0,0 +1,5 @@
+// Backend endpoints
+export const API_URL = 'http://localhost:8000/query'
+export const GRAPHQL_QUERY_NAME = 'queryTask'
+export const GRAPHQL_ADD_NAME = 'add'
+export const GRAPHQL_DELETE_NAME = "rm"
