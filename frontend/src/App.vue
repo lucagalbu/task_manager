1ba4c245b932b7d08e72a9412fcab25fa90a9ff9
Luca Galbusera <luca.galbusera@evotec.com>
2023-12-17 19:30:36 +0100

    [frontend] Bundled the components in the app
    I add all the components created so far to the main app component.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,1 +1,17 @@
-<template></template>
+<script setup lang="ts">
+import TodoList from '@/components/TodoList.vue'
+import TodoOptions from './components/TodoOptions.vue'
+</script>
+
+<template>
+    <div class="container-md">
+        <div class="card">
+            <div class="card-header">
+                <TodoOptions />
+            </div>
+            <div class="card-body">
+                <TodoList />
+            </div>
+        </div>
+    </div>
+</template>
