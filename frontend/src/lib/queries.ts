7a5487c038cca103e07c6a52ec86dfe6a53451c7
Luca Galbusera <luca.galbusera@evotec.com>
2023-12-17 19:15:08 +0100

    [frontend] Utility to create GraphQL requests
    Given a query/mutation string, it executes a GraphQL request.

--- frontend/src/lib/queries.ts
+++ frontend/src/lib/queries.ts
@@ -1,4 +1,4 @@
-import { GRAPHQL_QUERY_NAME, GRAPHQL_ADD_NAME, GRAPHQL_DELETE_NAME } from './config'
+import { GRAPHQL_QUERY_NAME, GRAPHQL_ADD_NAME, GRAPHQL_DELETE_NAME, API_URL } from './config'
 import type { graphQLTaskResponse } from './types'
 
 type GraphQLRespQuery = {
@@ -17,4 +17,19 @@ type GraphQLRespDelete = {
   data: {
     [GRAPHQL_DELETE_NAME]: graphQLTaskResponse
   }
+}
+
+
+const makeGraphQLRequest = function (body: string) {
+  const req_headers = new Headers()
+  req_headers.append('Content-Type', 'application/json')
+
+  const req_options: RequestInit = {
+    method: 'POST',
+    body: JSON.stringify({ query: body }),
+    headers: req_headers
+  }
+
+  const response = fetch(API_URL, req_options)
+  return response
 }
