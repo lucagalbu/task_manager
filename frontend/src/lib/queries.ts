833fa4255158d2aead1bb3be99907828a2a3b297
Luca Galbusera <luca.galbusera@evotec.com>
2023-12-17 19:20:11 +0100

    [frontend] Add a new task via GraphQL

--- frontend/src/lib/queries.ts
+++ frontend/src/lib/queries.ts
@@ -1,6 +1,6 @@
 import { GRAPHQL_QUERY_NAME, GRAPHQL_ADD_NAME, GRAPHQL_DELETE_NAME, API_URL } from './config'
-import { graphqlToTaskInput } from './converters'
-import type { TaskInput, graphQLTaskResponse } from './types'
+import { graphqlToTaskInput, statusToGraphQL } from './converters'
+import type { TaskInput, TaskOutput, graphQLTaskResponse } from './types'
 
 type GraphQLRespQuery = {
   data: {
@@ -60,4 +60,46 @@ export const queryAllTasks = async function (query_name: string): Promise<TaskIn
   const tasks_ql = response_json.data.queryTask
   const tasks_converted = tasks_ql.map((task) => graphqlToTaskInput(task))
   return tasks_converted
+}
+
+export const addTask = async function (
+  mutation_name: string,
+  task: TaskOutput
+): Promise<TaskInput | null> {
+  let mutation = `mutation ${mutation_name} {
+    ${GRAPHQL_ADD_NAME}(
+      title: "${task.title}"
+      status: ${statusToGraphQL(task.status)}`
+  if (task.description) {
+    mutation = mutation.concat(`\ndescription: "${task.description}"`)
+  }
+  if (task.date) {
+    mutation = mutation.concat(`\ndateTimestamp: "${task.date.toISOString().split("T")[0]}"`)
+  }
+  if (task.start_time) {
+    mutation = mutation.concat(`\nstartTimestamp: "${task.start_time}"`)
+  }
+  if (task.end_time) {
+    mutation = mutation.concat(`\nendTimestamp: "${task.end_time}"`)
+  }
+  if (task.goal) {
+    mutation = mutation.concat(`\ngoal: "${task.goal}"`)
+  }
+
+  mutation = mutation.concat(`
+            ) {
+                id,
+                title
+            }
+        }`)
+
+  const response = await makeGraphQLRequest(mutation)
+  if (!response.ok) {
+    return null
+  }
+
+  const response_json: GraphQLRespMutation = await response.json()
+  const added_task_ql = response_json.data[GRAPHQL_ADD_NAME]
+  const converted_task = graphqlToTaskInput(added_task_ql)
+  return converted_task
 }
